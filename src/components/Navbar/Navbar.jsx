d0143585f16b6f6481f88a228a3eefda98e7f5b3
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-08-18 11:59:45 +0530

    ordernow project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ordernow2 from "../../components/Navbar/ordernow2.png";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="logoContainer">
+      <img src={Ordernow2} alt="" />
+    </div>
+  );
+};
+
+export default Navbar;

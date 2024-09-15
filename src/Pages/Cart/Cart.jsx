1dc767d0f94d7b2fdb075ed29dbf7efa5d9c4762
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-09-15 17:44:57 +0530

    Add to cart problem

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,5 +1,4 @@
 import { food_list } from "../../assets/assets";
-// import cartItems from "../../components/FoodItems/FoodItems.jsx";
 
 const Cart = () => {
   return (
@@ -17,15 +16,12 @@ const Cart = () => {
       <br />
       <hr />
       {food_list.map((item, index) => {
-        return (
-          <div key={index} className="cart-items-title  cart-item-item">
-            <img src={item.image} alt="" />
-            <p>{item.name}</p>
-            {/* <p>{cartItems[item._id]}</p> */}
-            <p>{item.price}</p>
-            <p>X</p>
-          </div>
-        );
+        <div key={index} className="cart-items-title  cart-item-item">
+          <img src={item.image} alt="" />
+          <p>{item.name}</p>
+          <p>{item.price}</p>
+          <p>X</p>
+        </div>;
       })}
     </div>
   );
